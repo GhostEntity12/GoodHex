41a5be6f4f8b7d6a71a4f31506322d75e1091f6d
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2022-06-09 21:42:50 +1000

    Rename to prevent conflicts

--- Assets/Scripts/LoadSceneManager.cs
+++ Assets/Scripts/LoadSceneManager.cs
@@ -1,4 +1,4 @@
-public static class SceneManager 
+public static class LoadSceneManager 
 {
 	public static void LoadScene(int index) => UnityEngine.SceneManagement.SceneManager.LoadScene(index);
 	public static void LoadScene(string name) => UnityEngine.SceneManagement.SceneManager.LoadScene(name);
