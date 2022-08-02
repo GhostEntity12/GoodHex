138cd92a605ec51ba210f2a9f05c57501bd1ecf9
Josh Hehir <hehir24@gmail.com>
2022-08-02 14:44:32 +1000

    pickup item changes
    Basic pickup item script and testing in the Hallway scene

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform holdSpot;
+    public LayerMask pickUpMask;
+
+    public Vector3 Direction { get; set; }
+    private GameObject itemHolding;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (itemHolding)
+            {
+                itemHolding.transform.position = transform.position + Direction;
+                itemHolding.transform.parent = null;
+                if (itemHolding.GetComponent<Rigidbody>())
+                    itemHolding.GetComponent<Rigidbody>().isKinematic = true;
+                itemHolding = null;
+            }
+            else
+            {
+                Collider[] pickUpItems = Physics.OverlapSphere(transform.position + Direction, .4f, pickUpMask);
+                foreach (var pickUpItem in pickUpItems)
+                {
+                    itemHolding = pickUpItem.gameObject;
+                    itemHolding.transform.position = holdSpot.position;
+                    itemHolding.transform.parent = transform;
+                    if (itemHolding.GetComponent<Rigidbody>())
+                        itemHolding.GetComponent<Rigidbody>().isKinematic = false;
+                }
+            }
+        }
+    }
+}
