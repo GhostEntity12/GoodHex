a51df699fbdd06b8aa7cf360cfdc118cba538510
MGKineesh <mattken02@gmail.com>
2022-08-15 12:12:43 +1000

    Cat Kill implemented

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class KillZone : MonoBehaviour
+{
+    private GameObject rat;
+    NavMeshAgent agentCat;
+
+    void Start()
+    {
+        agentCat = transform.parent.GetComponent<NavMeshAgent>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Rat") // collider.TryGetComponent(out Rat rat))
+        {
+            rat = collider.gameObject;
+            Debug.Log(agentCat);
+            agentCat.isStopped = true;
+            Invoke("Kill", 2f);
+            //rat.Kill();
+        }
+    }
+
+    void Kill()
+    {
+        Destroy(rat);
+        agentCat.isStopped = false;
+    }
+}
