b582290c43b681df11fe15242eb26041a9a94833
MGKineesh <mattken02@gmail.com>
2022-08-28 22:47:18 +1000

    working on making ratagent move to center of hazard

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Hazard : MonoBehaviour
 {
+    //private NavMeshAgent ratAgent;
     void OnTriggerEnter(Collider collider)
     {
         if (collider.TryGetComponent(out Rat rat))
         {
+            //ratAgent = rat.GetComponent<NavMeshAgent>();
+            //ratAgent.destination = gameObject.transform.position;
+            //if (ratAgent.transform.position == gameObject.transform.position)
             rat.Kill(1f);
             //play animation
             //deactivate for few seconds
