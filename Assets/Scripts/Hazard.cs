4285cddb45f4e538638454117d01bb34af000813
MGKineesh <mattken02@gmail.com>
2022-09-05 14:22:55 +1000

    revamping hazard zone

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
@@ -10,39 +10,62 @@ public class Hazard : MonoBehaviour
     private bool captured;
     private Rat rat = null;
     private int selectedSpawn;
+    Bounds bounds;
+    private bool hazardActivated;
+    float timer;
     void Start()
     {
+        bounds = GetComponent<Collider>().bounds;
         captured = false;
         spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoints").Select(t => t.transform.position).ToArray();
+        timer = 3f;
     }
     void Update()
     {
-        if (rat)
+        if (hazardActivated)
         {
-            if (rat.NavAgent.remainingDistance < 0.1f)
+            timer -= Time.deltaTime;
+            if (timer <= 0)
             {
-                rat.Kill(1f);
-                captured = true;
-                rat = null;
+                HazardActivate();
+                
             }
-        }
-        if (captured)
-        {
-            Invoke("RespawnRats", 2f);
-            captured = false;
+            // if (rat.NavAgent.remainingDistance < 0.1f)
+            // {
+            //     rat.Kill();
+            //     captured = true;
+            //     rat = null;
+            // }
         }
     }
     void OnTriggerEnter(Collider collider)
     {
         if (!rat && collider.TryGetComponent(out rat))
         {
-            rat.SetDestination(position.position);
+            //rat.SetDestination(position.position);
+            hazardActivated = true;
         }
     }
 
-    void RespawnRats()
+    // void RespawnRats()
+    // {
+    //     selectedSpawn = Random.Range(0, spawnPoints.Length);
+    //     GameManager.Instance.RatManager.SpawnRats(spawnPoints[selectedSpawn]);
+    // }
+
+    void HazardActivate()
     {
-        selectedSpawn = Random.Range(0, spawnPoints.Length);
-        GameManager.Instance.RatManager.SpawnRats(spawnPoints[selectedSpawn]);
+        Queue<Rat> deadRats = new Queue<Rat>();
+        foreach (Rat rat in GameManager.Instance.RatManager.allRats)
+        {
+            if (bounds.Contains(rat.transform.position))
+            {
+                deadRats.Enqueue(rat);
+            }
+        }
+        while (deadRats.Count > 0)
+        {
+            deadRats.Dequeue().Kill();
+        }
     }
 }
--- Assets/Scripts/Rats/Rat.cs
+++ Assets/Scripts/Rats/Rat.cs
@@ -1,147 +1,154 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Linq;
 
 public class Rat : MonoBehaviour
 {
-	[SerializeField] SpriteRenderer graphic;
-	Animator anim;
+    [SerializeField] SpriteRenderer graphic;
+    Animator anim;
 
-	[Header("Navigation")]
-	private const float StoppingDistance = 0.15f;
-	[field: SerializeField] public NavMeshAgent NavAgent { get; private set; }
+    [Header("Navigation")]
+    private const float StoppingDistance = 0.15f;
+    [field: SerializeField] public NavMeshAgent NavAgent { get; private set; }
 
-	private float patience;
-	public bool AssignedToTask => GameManager.Instance.TaskManager.ratTasks.ContainsKey(this);
-	public bool ArrivedAtTask
-	{
-		get
-		{
-			TaskPoint tp = GameManager.Instance.TaskManager.GetTaskPoint(this);
-			if (tp == null) return false;
+    private float patience;
+    public bool AssignedToTask => GameManager.Instance.TaskManager.ratTasks.ContainsKey(this);
+    public bool ArrivedAtTask
+    {
+        get
+        {
+            TaskPoint tp = GameManager.Instance.TaskManager.GetTaskPoint(this);
+            if (tp == null) return false;
 
-			return Vector3.Distance(GameManager.Instance.TaskManager.GetTaskPoint(this).taskPosition, transform.position) < 0.1f;
-		}
-	}
-	[field: SerializeField] public bool Wandering { get; private set; }
+            return Vector3.Distance(GameManager.Instance.TaskManager.GetTaskPoint(this).taskPosition, transform.position) < 0.1f;
+        }
+    }
+    [field: SerializeField] public bool Wandering { get; private set; }
 
-	public RatData Info { get; private set; }
+    public RatData Info { get; private set; }
 
-	RatEmotes ratEmotes;
-	PickUp pickUp;
+    RatEmotes ratEmotes;
+    PickUp pickUp;
 
-	bool paused;
+    bool paused;
 
-	private bool isDead = false;
+    private bool isDead = false;
+    Vector3[] spawnPoints;
+    private int selectedSpawn;
 
-	private void Start()
-	{
-		GameManager.Pause += SetPaused;
+    private void Start()
+    {
+        GameManager.Pause += SetPaused;
 
-		ratEmotes = GetComponentInChildren<RatEmotes>();
-		anim = GetComponentInChildren<Animator>();
-		NavAgent = GetComponent<NavMeshAgent>();
-		pickUp = GetComponent<PickUp>();
+        ratEmotes = GetComponentInChildren<RatEmotes>();
+        anim = GetComponentInChildren<Animator>();
+        NavAgent = GetComponent<NavMeshAgent>();
+        pickUp = GetComponent<PickUp>();
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoints").Select(t => t.transform.position).ToArray();
 
-		if (NavMesh.SamplePosition(transform.position, out NavMeshHit hit, 0.1f, NavMesh.AllAreas))
-		{
-			transform.position = hit.position;
-		}
-		SetWander();
-	}
+        if (NavMesh.SamplePosition(transform.position, out NavMeshHit hit, 0.1f, NavMesh.AllAreas))
+        {
+            transform.position = hit.position;
+        }
+        SetWander();
+    }
 
-	private void Update()
-	{
-		if (paused) return;
+    private void Update()
+    {
+        if (paused) return;
 
-		anim.SetFloat("movementSpeed", NavAgent.velocity.magnitude);
-		anim.SetBool("wandering", Wandering);
-		anim.SetBool("occupied", ArrivedAtTask);
-		anim.SetBool("carrying", pickUp.IsHoldingItem);
+        anim.SetFloat("movementSpeed", NavAgent.velocity.magnitude);
+        anim.SetBool("wandering", Wandering);
+        anim.SetBool("occupied", ArrivedAtTask);
+        anim.SetBool("carrying", pickUp.IsHoldingItem);
 
-		// Reducing flicker
-		graphic.flipX = NavAgent.velocity.x switch
-		{
-			< 0.01f => true,
-			> -0.01f => false,
-			_ => graphic.flipX
-		};
+        // Reducing flicker
+        graphic.flipX = NavAgent.velocity.x switch
+        {
+            < 0.01f => true,
+            > -0.01f => false,
+            _ => graphic.flipX
+        };
 
-		if (!AssignedToTask)
-		{
-			if (Vector3.Distance(transform.position, NavAgent.destination) < StoppingDistance)
-			{
-				NavAgent.ResetPath();
-				patience -= Time.deltaTime;
-				if (patience <= 0)
-				{
-					SetWander();
-				}
-			}
-		}
-	}
-	public void AssignInfo(RatData ratInfo) => Info = ratInfo;
+        if (!AssignedToTask)
+        {
+            if (Vector3.Distance(transform.position, NavAgent.destination) < StoppingDistance)
+            {
+                NavAgent.ResetPath();
+                patience -= Time.deltaTime;
+                if (patience <= 0)
+                {
+                    SetWander();
+                }
+            }
+        }
+    }
+    public void AssignInfo(RatData ratInfo) => Info = ratInfo;
 
-	/// <summary>
-	/// Gives the rat a destination to move to
-	/// </summary>
-	/// <param name="position"></param>
-	public void SetDestination(Vector3 position)
-	{
-		NavAgent.SetDestination(position);
-		patience = Info.PatienceDuration;
-		NavAgent.speed = (pickUp.IsHoldingItem ? 1f : 2f) * Info.SpeedModifier;
-		Wandering = false;
-	}
+    /// <summary>
+    /// Gives the rat a destination to move to
+    /// </summary>
+    /// <param name="position"></param>
+    public void SetDestination(Vector3 position)
+    {
+        NavAgent.SetDestination(position);
+        patience = Info.PatienceDuration;
+        NavAgent.speed = (pickUp.IsHoldingItem ? 1f : 2f) * Info.SpeedModifier;
+        Wandering = false;
+    }
 
-	/// <summary>
-	/// Sets the rat to wander
-	/// </summary>
-	public void SetWander()
-	{
-		Wandering = true;
-		NavAgent.speed = 0.25f * Info.SpeedModifier;
-		Vector2 rand = Random.insideUnitCircle * Info.WanderRadius;
-		Vector3 offsetted = new(transform.position.x + rand.x, transform.position.y, transform.position.z + rand.y);
-		if (NavMesh.SamplePosition(offsetted, out NavMeshHit nMHit, 200f, NavMesh.AllAreas))
-		{
-			NavAgent.SetDestination(nMHit.position);
-		}
-	}
+    /// <summary>
+    /// Sets the rat to wander
+    /// </summary>
+    public void SetWander()
+    {
+        Wandering = true;
+        NavAgent.speed = 0.25f * Info.SpeedModifier;
+        Vector2 rand = Random.insideUnitCircle * Info.WanderRadius;
+        Vector3 offsetted = new(transform.position.x + rand.x, transform.position.y, transform.position.z + rand.y);
+        if (NavMesh.SamplePosition(offsetted, out NavMeshHit nMHit, 200f, NavMesh.AllAreas))
+        {
+            NavAgent.SetDestination(nMHit.position);
+        }
+    }
 
-	public void SetEmote(RatEmotes.Emotes emote) => ratEmotes.SetEmote(emote);
+    public void SetEmote(RatEmotes.Emotes emote) => ratEmotes.SetEmote(emote);
 
-	void SetPaused(bool paused)
-	{
-		this.paused = paused;
-		NavAgent.isStopped = paused;
-		anim.speed = paused ? 0 : 1;
-	}
+    void SetPaused(bool paused)
+    {
+        this.paused = paused;
+        NavAgent.isStopped = paused;
+        anim.speed = paused ? 0 : 1;
+    }
 
-	public void Kill(float pauseTime)
-	{
-		isDead = true;
-		NavAgent.isStopped = true;
-		GameManager.Instance.RatManager.RemoveRat(this);
-		// Leave corpse?
-		Invoke("Remove", pauseTime);
-	}
+    public void Kill()
+    {
+        isDead = true;
+        //NavAgent.isStopped = true;
+        GameManager.Instance.RatManager.RemoveRat(this);
+        Destroy(gameObject);
+		Invoke("RespawnRats", 3f);
+        // Leave corpse?
+        //Invoke("Remove", pauseTime);
+    }
 
-	public void Select()
-	{
-		graphic.color = Color.green;
-	}
+    public void Select()
+    {
+        graphic.color = Color.green;
+    }
 
-	public void Deselect()
-	{
-		graphic.color = Color.white;
-	}
-	private void OnDestroy()
-	{
-		GameManager.Pause -= SetPaused;
-	}
+    public void Deselect()
+    {
+        graphic.color = Color.white;
+    }
+    private void OnDestroy()
+    {
+        GameManager.Pause -= SetPaused;
+    }
 
-	private void Remove() 
-	{
-		Destroy(gameObject);
-	}
+    private void RespawnRats()
+    {
+        selectedSpawn = Random.Range(0, spawnPoints.Length);
+        GameManager.Instance.RatManager.SpawnRats(spawnPoints[selectedSpawn]);
+    }
 }
