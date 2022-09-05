6ac73f8edfeef332e701a4f6f4d4ffe6a5394e8a
MGKineesh <mattken02@gmail.com>
2022-09-05 10:59:27 +1000

    implemented respawning to hazard

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -1,16 +1,19 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Hazard : MonoBehaviour
 {
     [SerializeField] Transform position;
+    Vector3[] spawnPoints;
     private bool captured;
-    private Rat rat;
+    private Rat rat = null;
+    private int selectedSpawn;
     void Start()
     {
         captured = false;
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoints").Select(t => t.transform.position).ToArray();
     }
     void Update()
     {
@@ -19,15 +22,27 @@ public class Hazard : MonoBehaviour
             if (rat.NavAgent.remainingDistance < 0.1f)
             {
                 rat.Kill(1f);
+                captured = true;
                 rat = null;
             }
-        } 
+        }
+        if (captured)
+        {
+            Invoke("RespawnRats", 2f);
+            captured = false;
+        }
     }
     void OnTriggerEnter(Collider collider)
     {
         if (!rat && collider.TryGetComponent(out rat))
         {
-            rat.NavAgent.SetDestination(position.position);
+            rat.SetDestination(position.position);
         }
+    }
+
+    void RespawnRats()
+    {
+        selectedSpawn = Random.Range(0, spawnPoints.Length);
+        GameManager.Instance.RatManager.SpawnRats(spawnPoints[selectedSpawn]);
     }
 }
