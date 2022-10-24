076064d63032cefa4da5ab4e7c1fe54d9ae9428e
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2022-10-24 12:45:07 +1100

    Fixed animations, added object bob for decor on water

---
+++ Assets/Scripts/ObjectBob.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBob : MonoBehaviour
+{
+    float yCache;
+    [SerializeField] float bobSpeed;
+    [SerializeField] float bobHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yCache = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, yCache + Mathf.Sin((Time.time + transform.position.x) * bobSpeed) * bobHeight, transform.position.z);
+    }
+}
